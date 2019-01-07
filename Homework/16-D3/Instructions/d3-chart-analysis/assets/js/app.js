2807c02dd94336d1b6355fe59a2cae506c43c75d
Andres <andresgpotdevin@gmail.com>
2019-01-07 17:12:38 -0500

    finalizing D3 Homework

--- Homework/16-D3/Instructions/d3-chart-analysis/assets/js/app.js
+++ Homework/16-D3/Instructions/d3-chart-analysis/assets/js/app.js
@@ -1,11 +1,13 @@
 var svgWidth = 800;
 var svgHeight = 450;
+
 var margin = {
   top: 20,
   right: 40,
   bottom: 60,
   left: 100
 };
+
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
@@ -47,9 +49,8 @@ d3.csv("assets/data/data.csv")
       chartGroup.append("g")
         .call(leftAxis);
 
-       // Step 5: Create Circles
-        // ==============================
-        var circlesGroup = chartGroup.selectAll("circle")
+       //Create Circles
+      var circlesGroup = chartGroup.selectAll("circle")
         .data(data)
         .enter()
         .append("circle")
@@ -59,8 +60,18 @@ d3.csv("assets/data/data.csv")
         .attr("fill", "pink")
         .attr("opacity", ".5");
 
-        // Step 6: Initialize tool tip
-        // ==============================
+      chartGroup.selectAll("text")
+        .data(data)
+        .enter()
+        .append("text")
+        .attr("x",(d) => xLinearScale(d.income))
+        .attr("y",(d) => yLinearScale(d.obesity))
+        .attr("text-anchor", "middle")
+        .attr("font-size", "12px")
+        .style("fill","Black")
+        .text((d) => d.abbr);
+
+        //Initialize tool tip
         var toolTip = d3.tip()
           .attr("class", "tooltip")
           .offset([80, -60])
@@ -68,46 +79,31 @@ d3.csv("assets/data/data.csv")
             return (`${d.state}<br>Income: ${d.income}<br>Obesity: ${d.obesity}`);
           });
 
-        // Step 7: Create tooltip in the chart
-        // ==============================
+
+        //Create tooltip in the chart
         chartGroup.call(toolTip);
 
-        // Step 8: Create event listeners to display and hide the tooltip
-        // ==============================
         circlesGroup.on("click", function(data) {
           toolTip.show(data, this);
         })
-          // onmouseout event
           .on("mouseout", function(data, index) {
             toolTip.hide(data);
           });
 
-          svg.selectAll("text")
-         .data(data)
-         .enter()
-         .append("text")
-         .attr("x",d=> {return xLinearScale(d.income)})
-         .attr("y",d=> {return yLinearScale(d.obesity)})
-         .attr("text-anchor", "middle")
-         .attr("font-size", "12px")
-         .attr("class", "stateText")
-         .text(function(data,index){
-             return data.abbr;
-           });
 
         // Create axes labels
         chartGroup.append("text")
           .attr("transform", "rotate(-90)")
           .attr("y", 0 - margin.left + 40)
-          .attr("x", 0 - (height / 2))
+          .attr("x", 0 - (height * 1.2 / 2))
           .attr("dy", "1em")
           .attr("class", "axisText")
-          .text("Number of Billboard 100 Hits");
+          .text("Obesity per State");
 
         chartGroup.append("text")
-          .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+          .attr("transform", `translate(${width * .7 / 2}, ${height + margin.top + 20})`)
           .attr("class", "axisText")
-          .text("Hair Metal Band Hair Length (inches)");
+          .text("Average Income per State");
   })
   .catch(function(error){
      // handle error
