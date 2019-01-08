4e1e4c34a9747845634da4546e4b6ff6bf5ec755
Andres <andresgpotdevin@gmail.com>
2019-01-08 00:12:24 -0500

    created app

--- websites/space-x/app.py
+++ websites/space-x/app.py
@@ -1,9 +1,18 @@
 from flask import Flask, request, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template('index.html')
+    return render_template('home.html')
+
+@app.route('/missions')
+def missions():
+        return render_template('/missions.html')
+
+@app.route('/about')
+def about():
+        return render_template('/about.html')
 
 if __name__ == '__main__':
    app.run(debug = True)
